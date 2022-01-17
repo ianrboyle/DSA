8bb60a69088afa001242893b825d5c0e43766078
Ian Boyle <irboyle21@gmail.com>
2022-01-17 10:33:13 -0500

    adds recrusive factorial function

--- recursion/first-function.js
+++ recursion/first-function.js
@@ -1,4 +1,13 @@
 function sumRange(num) {
-  if (num === 1) return 1;
+  if (num === 1) {
+    return 1;
+  }
   return num + sumRange(num - 1);
+}
+
+function recursiveFactorial(num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * recursiveFactorial(num - 1);
 }
